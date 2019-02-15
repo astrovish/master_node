c13ef82f54a58d9ed07837023941b80647145697
vishal <astrovish@gmail.com>
2019-02-15 06:34:03 +0530

    Added events example, installed package: fs to work with file, url to work with urls, multer to upload files in a form

--- index.js
+++ index.js
@@ -5,15 +5,31 @@ const http = require("http");
 const port = 3000;
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const dateMod = require("./custom_modules/datemod");
+const events = require("events");
+const formidable = require("formidable");
+const multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
+////////////////////////////////////////////
+var eventEmitter = new events.EventEmitter();
+
+var burnoutHandler = () => {
+  console.log("WORK HARD!!! .......TILL YOU EXHAUSTED!!!");
+};
+
+eventEmitter.on("BurnOut", burnoutHandler);
+eventEmitter.emit("BurnOut");
+////////////////////////////////////////////
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-const studied = [];
+const studie = [];
 
 app.get("/", (req, res) => {
   //res.send("Home Page");
-  res.render("landing");
+  res.render("landing", { currDateTime: dateMod.currDateTime() });
 });
 
 app.get("/about", (req, res) => {
@@ -21,14 +37,27 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.post("/features", (req, res) => {
+app.post("/features", upload.field, (req, res) => {
   var name = req.body.featureName;
   var image = req.body.relatedImage;
   var newFeature = { name: name, image: image };
-  studied.push(newFeature);
-  res.render("features", { studied: studied });
+  studie.push(newFeature);
+  res.render("features", { studied: studie });
 });
 
 app.listen(3000, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
+
+/*
+const url = require('url');
+const fs = require('fs');
+
+var q = url.parse(req.url, true);
+fs.readFile();
+fs.writeFile();
+fs.appendFile();
+fs.open(filename, flag);
+fs.unlink();
+fs.rename();
+*/
